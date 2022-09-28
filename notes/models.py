043d6bd4ff2e6af44248c74ee6ffa51898c5d162
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-09-28 16:57:04 -0300

    Tag property

--- notes/models.py
+++ notes/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=200)
     details = models.TextField()
+    tag = models.CharField(max_length=20, default='')
     def __str__(self):
-        return '{}. {}'.format(self.id, self.title)
+        return '{}. {} #{}'.format(self.id, self.title, self.tag)
--- notes/views.py
+++ notes/views.py
@@ -7,7 +7,10 @@ def index(request):
     if request.method == 'POST':
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
-        new_note = Note.objects.create(title=title, details=content)
+        tag = request.POST.get('tag')
+        if not tag.startswith('#'):
+            tag = '#' + tag
+        new_note = Note.objects.create(title=title, details=content, tag=tag)
         new_note.save()
         return redirect('index')
     else:
